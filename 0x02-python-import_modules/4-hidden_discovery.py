fa867883abd76fae6bc356dbff98ef13a34bc4b9
lowercaselife <dansonkalaghe@gmail.com>
2021-09-09 09:27:53 -0700

    python import/modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import hidden_4
-    names = dir(hidden_4)
-    for i in names:
-        if i[:2] != "__":
-            print(i)
+    temp = dir(hidden_4)
+    for i in range(len(temp)):
+        for j in range(len(temp[i])):
+            if (temp[i][j] == '_' and temp[i][j+1] == '_'):
+                break
+            else:
+                print(temp[i])
+                break
