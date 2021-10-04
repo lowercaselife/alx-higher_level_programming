16d73afc98f5f99e1c43e1411a7c360bdb2d3455
lowercaselife <dansonkalaghe@gmail.com>
2021-10-04 07:36:43 -0700

    classes/objects and test files

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+""" File name : 0-rectangle.py
+    Simple rectangle : an empty class Rectangle that defines a rectangle
+    It is not allowed to import any module
+"""
+
+
+class Rectangle(object):
+    """Rectangle: Define new class"""
+    pass
