f9ba3e3868ed27c746cf2dbc341121b59e2e26e9
lowercaselife <dansonkalaghe@gmail.com>
2021-10-12 09:15:43 -0700

    input/output

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 4-append_write.py
-# Carlos Barros <1543@holbertonschool.com>
 """ File name : 4-append_write.py
 """
 
--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 5-to_json_string.py
-# Carlos Barros <1543@holbertonschool.com>
 """ File name : 5-to_json_string.py
 """
 import json
