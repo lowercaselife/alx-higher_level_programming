c3fd4ed2e8f21b33f005e9dfbcbff7242a07fabc
Danson <dansonkalaghe@gmail.com>
2021-11-09 00:01:36 -0800

    error correction

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 
+
 from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-new_list = []
-
+filename = "add_item.json"
 try:
-    list_file = load_from_json_file("add_item.json")
-    for i in list_file:
-        new_list.append(i)
+    the_list = load_from_json_file(filename) + argv[1:]
+    save_to_json_file(the_list, filename)
 except:
-    new_list = []
-
-for i in range(len(argv)):
-    if i != 0:
-        new_list.append(argv[i])
-save_to_json_file(new_list, "add_item.json")
+    save_to_json_file(argv[1:], filename)
