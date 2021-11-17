1303c453051020daf309a54d1bfabae3e536698a
lowercaselife <dansonkalaghe@gmail.com>
2021-11-17 14:22:45 +0000

    hot fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,9 @@
--- Create the MySQL server user user_0d_1
+-- Creates the user user_0d_1 with all privileges.
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+    ON *.*
+    TO 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
-
