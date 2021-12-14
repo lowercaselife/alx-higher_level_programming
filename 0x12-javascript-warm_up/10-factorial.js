3b76335d539f6fe50a58dd6c5ea446338599c35e
lowercaselife <dansonkalaghe@gmail.com>
2021-12-14 15:59:04 +0000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -5,8 +5,9 @@ function factorial (number) {
   }
   if (number < 0) {
     return -1;
+  }
   if (number === 0) {
-    return 1
+    return 1;
   } else {
     return number * factorial(number - 1);
   }
