85a33a84eb356498fdff736d1e23a7d0c9826506
lowercaselife <dansonkalaghe@gmail.com>
2021-12-14 17:51:57 +0000

    reverse

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 // script reverses a list
-exports.esrever = function (input) {
-  const ret = new Array;
-  for (let i = input.length - 1; i >= 0; i--) {
-    ret.push(input[i]);
-  }
-  return ret;
+exports.esrever = function (list) {
+  return list.map((item, idx) => list[list.length - 1 - idx]);
 };
